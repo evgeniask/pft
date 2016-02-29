fddc2513b7ffc0c266598dedacf01d5ad578c46c
evgeniask <evisunny@yandex.ru>
2016-02-29 21:22:30 +0300

    Square calculation for rectangle and square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -2,8 +2,18 @@ package ru.stqa.pft.sandbox;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("test text");
+        hello("world");
+        hello("user");
         
+        Square s = new Square(6);
+        System.out.println("Площаль квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(2, 3);
+        System.out.println("Площаль прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
     }
 
 }
