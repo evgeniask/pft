0562ef50ec89a6b537bb53bf3a525c83d880186c
evgeniask <evisunny@yandex.ru>
2016-02-29 15:41:35 +0300

    Modified main function to refer to method in the class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -3,11 +3,11 @@ package ru.stqa.pft.sandbox;
 public class Distance {
 
     public static void main(String[] args) {
-        Point p = new Point();
+
         Point p1 = new Point(2, 3);
         Point p2 = new Point(-5, -1);
 
-        System.out.println("Distance between two points with coordinates " + "(" + p1.x + ", " + p1.y + ") and " + "(" + p2.x + ", " + p2.y + ") equals to " + p.distance(p1,p2));
+        System.out.println("Distance between two points with coordinates " + "(" + p1.x + ", " + p1.y + ") and " + "(" + p2.x + ", " + p2.y + ") equals to " + Point.distance(p1,p2));
     }
 
 }
