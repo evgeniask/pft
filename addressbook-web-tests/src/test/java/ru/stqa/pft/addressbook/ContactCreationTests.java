c209bee0e82f9e9e3632c2590c8c66fafcec9e98
evgeniask <evisunny@yandex.ru>
2016-03-01 22:49:05 +0300

    Contact creation test added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -17,28 +17,28 @@ public class ContactCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void testContactCreation() {
         initContactCreation();
-        fillContactForm();
+        fillContactForm(new ContactData("Ivan", "Ivanov", "SPb, Noname street 77-35", "9934578", "9256883444", "ivan.ivanov@ivanov.ivan", "ivan.ivanov@ttt.com"));
         submitContactForm();
-        returntoHomePage();
+        returnToHomePage();
     }
 
-    private void returntoHomePage() {
+    private void returnToHomePage() {
         wd.findElement(By.linkText("home page")).click();
     }
 
@@ -46,28 +46,28 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Ivan");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Ivanov");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("SPb, Noname street 77-35");
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("9934578");
+        wd.findElement(By.name("home")).sendKeys(contactData.getHomephone());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("9256883444");
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilephone());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("ivan.ivanov@ivanov.ivan");
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
         wd.findElement(By.name("email2")).click();
         wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys("ivan.ivanov@ttt.com");
+        wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
     }
 
     private void initContactCreation() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String homephone;
+    private final String mobilephone;
+    private final String email;
+    private final String email2;
+
+    public ContactData(String firstname, String lastname, String address, String homephone, String mobilephone, String email, String email2) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.homephone = homephone;
+        this.mobilephone = mobilephone;
+        this.email = email;
+        this.email2 = email2;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public String getMobilephone() {
+        return mobilephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+}
