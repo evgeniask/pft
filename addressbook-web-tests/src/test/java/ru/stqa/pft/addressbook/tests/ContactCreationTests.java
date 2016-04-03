b1220c6406cf76d85da88738480f304a5bedb71b
evgeniask <evisunny@yandex.ru>
2016-04-03 16:15:25 +0300

    Added Data providers for groups and contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,26 +1,40 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+                    .withHomephone(split[3]).withMobilephone(split[4]).withWorkphone(split[5])
+                    .withEmail(split[6]).withEmail2(split[7]).withEmail3(split[8])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.goTo().homePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/picture.png");
-        ContactData contact = new ContactData()
-                .withFirstname("Ivan").withLastname("Ivanov").withAddress("SPb, Noname street 77-35")
-                .withHomephone("(993)4578").withMobilephone("925-6883-444").withWorkphone("44 55 77")
-                .withEmail("ivan.ivanov@ivanov.ivan").withEmail2("ivan.ivanov@ttt.com")
-                .withEmail3("ivan.ivanov@third.com").withGroup("test1").withPhoto(photo);
+        //File photo = new File("src/test/resources/picture.png");
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
@@ -28,18 +42,18 @@ public class ContactCreationTests extends TestBase {
                 (before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 
-    @Test(enabled = false)
-    public void testBadContactCreation() {
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        ContactData contact = new ContactData()
-                .withFirstname("Ivan'").withLastname("Ivanov").withAddress("SPb, Noname street 77-35")
-                .withHomephone("(993)4578").withMobilephone("925-6883-444").withWorkphone("44 55 77")
-                .withEmail("ivan.ivanov@ivanov.ivan").withEmail2("ivan.ivanov@ttt.com")
-                .withEmail3("ivan.ivanov@third.com").withGroup("test1");
-        app.contact().create(contact);
-        assertThat(app.contact().count(), equalTo(before.size()));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(before));
-    }
+//    @Test(enabled = false)
+//    public void testBadContactCreation() {
+//        app.goTo().homePage();
+//        Contacts before = app.contact().all();
+//        ContactData contact = new ContactData()
+//                .withFirstname("Ivan'").withLastname("Ivanov").withAddress("SPb, Noname street 77-35")
+//                .withHomephone("(993)4578").withMobilephone("925-6883-444").withWorkphone("44 55 77")
+//                .withEmail("ivan.ivanov@ivanov.ivan").withEmail2("ivan.ivanov@ttt.com")
+//                .withEmail3("ivan.ivanov@third.com").withGroup("test1");
+//        app.contact().create(contact);
+//        assertThat(app.contact().count(), equalTo(before.size()));
+//        Contacts after = app.contact().all();
+//        assertThat(after, equalTo(before));
+//    }
 }
