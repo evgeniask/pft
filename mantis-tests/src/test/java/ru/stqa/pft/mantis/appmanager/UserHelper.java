bb8f7615fd6b4c18e576cac6fc10dc55ae88344d
evgeniask <evisunny@yandex.ru>
2016-04-19 22:11:32 +0300

    Implemented reset password method

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/UserHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/UserHelper.java
@@ -24,8 +24,26 @@ public class UserHelper extends HelperBase {
         app.registration().finish(confirmationLink, password);
     }
 
-    public void initManageUserByName(String username) {
+    public void uiLogin(String username, String password) {
+        wd.get(app.getProperty("web.baseUrl") + "/login_page.php");
+        type(By.name("username"), username);
+        type(By.name("password"), password);
+        click(By.cssSelector("input[value='Login']"));
+    }
+
+    public void initManageUserByName(UserData user) {
         wd.get(app.getProperty("web.baseUrl") + "/manage_user_page.php");
-        wd.findElement((By.linkText(String.format("&s", username)))).click();
+        wd.findElement(By.cssSelector(String.format("a[href='manage_user_edit_page.php?user_id=%s']", user.getId()))).click();
+    }
+
+    public void resetPassword(UserData user, String newPassword) throws IOException, MessagingException {
+        initManageUserByName(user);
+        click(By.cssSelector("input[value='Reset Password']"));
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+        String confirmationLink = app.mail().findChangePasswordLink(mailMessages, user.getEmail());
+        wd.get(confirmationLink);
+        type(By.name("password"), newPassword);
+        type(By.name("password_confirm"), newPassword);
+        click(By.cssSelector("input[value='Update User']"));
     }
 }
