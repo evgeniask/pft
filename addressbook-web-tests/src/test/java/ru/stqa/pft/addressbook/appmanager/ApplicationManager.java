3ee6884ebff5bae2938988a66be2acb03f05b219
evgeniask <evisunny@yandex.ru>
2016-04-24 21:36:49 +0300

    Implemented ability to set platform

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -35,21 +35,22 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
         dbHelper = new DbHelper();
-if ("".equals(properties.getProperty("selenium.server"))) {
-    if (browser.equals(BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-        wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-        wd = new InternetExplorerDriver();
-    }
-} else {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setBrowserName(browser);
-    wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
-}
+
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+        }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
